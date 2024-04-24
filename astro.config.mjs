424459336b33db1c46a07c28f33dd521e5bf7ff2
angelurrutdev <angeljurrut@gmail.com>
2024-04-23 20:44:43 -0400

    add dependencies to upload a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
+  adapter: vercel()
 });
